35c97819fff4ae783696665244e9c4ead64b42b2
Atay Özdemir <atayozdemir01@gmail.com>
2025-07-29 10:26:48 +0300

    Initial Commit

---
+++ command strategy beta version/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  command strategy beta version
+//
+//  Created by atay özdemir on 29.07.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
